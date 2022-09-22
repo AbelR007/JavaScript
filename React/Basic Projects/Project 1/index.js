08505f57c1a6cddc97d9373ab5bcad2ef2a902e3
AbelR007 <abelroi007@gmail.com>
2022-09-22 23:14:33 +0530

    v1.4 - React - Bug Fixes
    Files containing redundant code fixed,
    Basic websites updated, and code reevaluated

---
+++ React/Basic Projects/Project 1/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const page = (
+    <h1>Testing.</h1>
+)
+
+// ReactDOM.render(page, document.getElementById("root"))
+ReactDOM.createRoot(document.getElementById("root")).render(page)
