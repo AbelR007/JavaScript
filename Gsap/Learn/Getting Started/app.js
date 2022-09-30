89a65d2cef1e3c352ddc6e957a85362cf995aadb
AbelR007 <abelroi007@gmail.com>
2022-09-30 22:47:17 +0530

    v1.4.4 - Basics

--- Gsap/Learn/Getting Started/app.js
+++ Gsap/Learn/Getting Started/app.js
@@ -1,14 +1,29 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
+// GSAP time!
 gsap.to(".square",{
   x: 700, // position to move towards
   duration: 3, // duration or time delay for that specific movement
+  
   scrollTrigger: { // object
+
     trigger: ".square", // triggers when the .square appears
-    start: "top 30%",
-    end: "center 20%",
-    markers: true,
-    toggleClass: "red",
+    
+    // Start & End of the element, can be seen when used markers
+    start: "top 80%",
+    end: "top 30%",
+
+    // performs actions as you... onEnter, onLeave, onEnterBack, onLeaveBack
+    toggleActions: "restart none none none",
+
+    // Used to pin elements from start till end
+    pin: true,
+
+    // Allows movement of element with scroll bar movement
+    scrub: 4,
+
+    // Mostly used by developers for understanding purposes
+    markers: true
   }
 })
